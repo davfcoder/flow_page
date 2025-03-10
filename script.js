37bf80e29bce68e09c0048a3c6e338a43de8837e
jhansarmiento <jhancsn1@gmail.com>
2025-03-10 10:05:05 -0500

    Carousel terminado

--- script.js
+++ script.js
@@ -58,27 +58,39 @@ close.onclick = function() {
 
 // Carousel
 
-document.addEventListener("DOMContentLoaded", function () {
-  const carousel = document.getElementById("carousel");
-  const prevBtn = document.getElementById("prev");
-  const nextBtn = document.getElementById("next");
-  let index = 0; // Índice actual de la imagen mostrada
-
-  // Función para actualizar el desplazamiento del carrusel
-  function updateCarousel() {
-    const offset = -index * 100; // Calcula el desplazamiento en porcentaje
-    carousel.style.transform = `translateX(${offset}%)`; // Aplica la transformación
-  }
-
-  // Evento para el botón "Siguiente"
-  nextBtn.addEventListener("click", function () {
-    index = (index + 1) % 6; // Avanza a la siguiente imagen, vuelve a la primera al final
-    updateCarousel(); // Llama a la función para actualizar la vista
-  });
-
-  // Evento para el botón "Anterior"
-  prevBtn.addEventListener("click", function () {
-    index = (index - 1 + 5) % 6; // Retrocede a la imagen anterior, vuelve a la última si es necesario
-    updateCarousel();
-  });
-});
+let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    
+    let active = 3;
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${180*stt}px) scale(${1 - 0.3*stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-180*stt}px) scale(${1 - 0.3*stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+    loadShow();
+    next.onclick = function(){
+        active = active + 1 < items.length ? active + 1 : active;
+        loadShow();
+    }
+    prev.onclick = function(){
+        active = active - 1 >= 0 ? active - 1 : active;
+        loadShow();
+    }
